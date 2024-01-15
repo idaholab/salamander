25672aeb8572179f6aea49f45cb7df746f5ecb93
Logan Harbour <loganhharbour@gmail.com>
2024-01-15 13:28:59 -0700

    Use new moose main
    refs idaholab/moose#25994

--- src/main.C
+++ src/main.C
@@ -8,29 +8,12 @@
 //* https://www.gnu.org/licenses/lgpl-2.1.html
 
 #include "SalamanderTestApp.h"
-#include "MooseInit.h"
-#include "Moose.h"
-#include "MooseApp.h"
-#include "AppFactory.h"
+#include "MooseMain.h"
 
-// Create a performance log
-PerfLog Moose::perf_log("Salamander");
-
-// Begin the main program.
 int
 main(int argc, char * argv[])
 {
-  // Initialize MPI, solvers and MOOSE
-  MooseInit init(argc, argv);
-
-  // Register this application's MooseApp and any it depends on
-  SalamanderTestApp::registerApps();
-
-  // Create an instance of the application and store it in a smart pointer for easy cleanup
-  std::shared_ptr<MooseApp> app = AppFactory::createAppShared("SalamanderTestApp", argc, argv);
-
-  // Execute the application
-  app->run();
+  Moose::main<SalamanderTestApp>(argc, argv);
 
   return 0;
 }
