532bff4a34acf20648f0805912eea3a5f9f6a763
Grayson Gall <ggall713@gmail.com>
2024-05-21 16:09:32 -0600

    particle initialization is done in all 3 dimensions

--- 2D-tri/run_particle_study.py
+++ 2D-tri/run_particle_study.py
@@ -1,3 +1,4 @@
+
 import mooseutils
 import os
 from random import randint
@@ -9,30 +10,27 @@ supress_output = False
 
 
 
-refine_ments = [0, 1, 2, 3]
+particles_per_element = [10, 100, 1000, 10000]
 
-ppe = 1000
-
-num_seeds = 10
-
-top_folder = f"{ppe:d}ppe"
+num_trials=10
+mpi=24
+top_folder = "particle_study"
 
 if not os.path.exists(top_folder):
   os.mkdir(top_folder)
 
-for i in range(num_seeds):
+for i in range(num_trials):
      curr_dir = f"{top_folder}/run_{i:d}"
 
      if not os.path.exists(curr_dir):
           os.mkdir(curr_dir)
 
-     for n in refine_ments:
+     for n in particles_per_element:
           a = ["-i", input_file,
                "--allow-test-objects",
-               f"Mesh/uniform_refine={n:d}",
-               f"Outputs/csv/file_base={curr_dir}/{n:d}_elems",
-               f"Samplers/sampler/seed={randint(0, 34912348):d}",
-               f"UserObjects/study/particles_per_elememt{ppe:d}"
+               "Mesh/gmg/nx=100",
+               f"Outputs/csv/file_base={curr_dir}/{n:d}_ppe",
+               f"UserObjects/initializer/seed={randint(0,1329408):d}",
+               f"GlobalParams/particles_per_element={n:d}"
                ]
-          mpi=20
           mooseutils.run_executable(executeable, *a, mpi=mpi, suppress_output=supress_output)
