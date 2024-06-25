gsgall/fenix

reorganizing some files to make the structure better

src/userobjects/BoundingBoxParticleInitializer.C
@@ -13,19 +13,19 @@
 //* ALL RIGHTS RESERVED
 //*
 
-#include "BoundingBoxInitializer.h"
+#include "BoundingBoxParticleInitializer.h"
 #include "MooseRandom.h"
 #include "ElementSampler.h"
 #include "Distribution.h"
 
-registerMooseObject("FenixApp", BoundingBoxInitializer);
+registerMooseObject("FenixApp", BoundingBoxParticleInitializer);
 
 InputParameters
-BoundingBoxInitializer::validParams()
+BoundingBoxParticleInitializer::validParams()
 {
-  auto params = ParticlesPerElementInitializer::validParams();
+  auto params = PerElementParticleInitializer::validParams();
   params.addClassDescription(
-      "This initializer performs the same task as [ParticlesPerElementInitializer.md], however, "
+      "This initializer performs the same task as [PerElementParticleInitializer.md], however, "
       "only particles that exist within the bounding box are created.");
   params.addRequiredParam<Point>("bottom_left", "The bottom left corner of the bounding box");
   params.addRequiredParam<Point>("top_right", "The top right corner of the bounding box");
@@ -33,8 +33,8 @@ BoundingBoxInitializer::validParams()
   return params;
 }
 
-BoundingBoxInitializer::BoundingBoxInitializer(const InputParameters & parameters)
-  : ParticlesPerElementInitializer(parameters),
+BoundingBoxParticleInitializer::BoundingBoxParticleInitializer(const InputParameters & parameters)
+  : PerElementParticleInitializer(parameters),
     _bottom_left(getParam<Point>("bottom_left")),
     _top_right(getParam<Point>("top_right")),
     _planes({{-1, 0, 0, _bottom_left(0)},
@@ -63,7 +63,7 @@ BoundingBoxInitializer::BoundingBoxInitializer(const InputParameters & parameter
 }
 
 std::vector<InitialParticleData>
-BoundingBoxInitializer::getParticleData() const
+BoundingBoxParticleInitializer::getParticleData() const
 {
 
   // first collect the elements that we will place particles in

src/userobjects/PerElementParticleInitializer.C
@@ -8,16 +8,16 @@
 //* Licensed under LGPL 2.1, please see LICENSE for details
 //* https://www.gnu.org/licenses/lgpl-2.1.html
 
-#include "ParticlesPerElementInitializer.h"
+#include "PerElementParticleInitializer.h"
 #include "MooseRandom.h"
 #include "ElementSampler.h"
 
-registerMooseObject("FenixApp", ParticlesPerElementInitializer);
+registerMooseObject("FenixApp", PerElementParticleInitializer);
 
 InputParameters
-ParticlesPerElementInitializer::validParams()
+PerElementParticleInitializer::validParams()
 {
-  auto params = InitializerBase::validParams();
+  auto params = ParticleInitializerBase::validParams();
   params.addClassDescription(
       "Particle initializer that uniformly distributes a specified number of particles per "
       "element and calculates the corresponding particle weight based on the requested number "
@@ -33,15 +33,15 @@ ParticlesPerElementInitializer::validParams()
   return params;
 }
 
-ParticlesPerElementInitializer::ParticlesPerElementInitializer(const InputParameters & parameters)
-  : InitializerBase(parameters),
+PerElementParticleInitializer::PerElementParticleInitializer(const InputParameters & parameters)
+  : ParticleInitializerBase(parameters),
     _number_density(getParam<Real>("number_density")),
     _particles_per_element(getParam<unsigned int>("particles_per_element"))
 {
 }
 
 std::vector<InitialParticleData>
-ParticlesPerElementInitializer::getParticleData() const
+PerElementParticleInitializer::getParticleData() const
 {
 
   // counting the number of elements this process is responsible for

idaholab/salamander

src/userobjects/BoundingBoxParticleInitializer.C
@@ -13,19 +13,19 @@
 //* ALL RIGHTS RESERVED
 //*
 
-#include "BoundingBoxInitializer.h"
+#include "BoundingBoxParticleInitializer.h"
 #include "MooseRandom.h"
 #include "ElementSampler.h"
 #include "Distribution.h"
 
-registerMooseObject("SalamanderApp", BoundingBoxInitializer);
+registerMooseObject("SalamanderApp", BoundingBoxParticleInitializer);
 
 InputParameters
-BoundingBoxInitializer::validParams()
+BoundingBoxParticleInitializer::validParams()
 {
-  auto params = ParticlesPerElementInitializer::validParams();
+  auto params = PerElementParticleInitializer::validParams();
   params.addClassDescription(
-      "This initializer performs the same task as [ParticlesPerElementInitializer.md], however, "
+      "This initializer performs the same task as [PerElementParticleInitializer.md], however, "
       "only particles that exist within the bounding box are created.");
   params.addRequiredParam<Point>("bottom_left", "The bottom left corner of the bounding box");
   params.addRequiredParam<Point>("top_right", "The top right corner of the bounding box");
@@ -33,8 +33,8 @@ BoundingBoxInitializer::validParams()
   return params;
 }
 
-BoundingBoxInitializer::BoundingBoxInitializer(const InputParameters & parameters)
-  : ParticlesPerElementInitializer(parameters),
+BoundingBoxParticleInitializer::BoundingBoxParticleInitializer(const InputParameters & parameters)
+  : PerElementParticleInitializer(parameters),
     _bottom_left(getParam<Point>("bottom_left")),
     _top_right(getParam<Point>("top_right")),
     _planes({{-1, 0, 0, _bottom_left(0)},
@@ -63,7 +63,7 @@ BoundingBoxInitializer::BoundingBoxInitializer(const InputParameters & parameter
 }
 
 std::vector<InitialParticleData>
-BoundingBoxInitializer::getParticleData() const
+BoundingBoxParticleInitializer::getParticleData() const
 {
 
   // first collect the elements that we will place particles in

src/userobjects/PerElementParticleInitializer.C
@@ -13,16 +13,16 @@
 //* ALL RIGHTS RESERVED
 //*
 
-#include "ParticlesPerElementInitializer.h"
+#include "PerElementParticleInitializer.h"
 #include "MooseRandom.h"
 #include "ElementSampler.h"
 
-registerMooseObject("SalamanderApp", ParticlesPerElementInitializer);
+registerMooseObject("SalamanderApp", PerElementParticleInitializer);
 
 InputParameters
-ParticlesPerElementInitializer::validParams()
+PerElementParticleInitializer::validParams()
 {
-  auto params = InitializerBase::validParams();
+  auto params = ParticleInitializerBase::validParams();
   params.addClassDescription(
       "Particle initializer that uniformly distributes a specified number of particles per "
       "element and calculates the corresponding particle weight based on the requested number "
@@ -38,15 +38,15 @@ ParticlesPerElementInitializer::validParams()
   return params;
 }
 
-ParticlesPerElementInitializer::ParticlesPerElementInitializer(const InputParameters & parameters)
-  : InitializerBase(parameters),
+PerElementParticleInitializer::PerElementParticleInitializer(const InputParameters & parameters)
+  : ParticleInitializerBase(parameters),
     _number_density(getParam<Real>("number_density")),
     _particles_per_element(getParam<unsigned int>("particles_per_element"))
 {
 }
 
 std::vector<InitialParticleData>
-ParticlesPerElementInitializer::getParticleData() const
+PerElementParticleInitializer::getParticleData() const
 {
 
   // counting the number of elements this process is responsible for