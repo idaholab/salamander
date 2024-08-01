aadf2f7ff6ccfbe5da7ffd903f46c51bd5a269c7
Grayson Gall <ggall713@gmail.com>
2024-07-31 20:10:46 -0600

    Extracted residual accumulator base class
    - useful for simplifying future accumulators

---
+++ include/userobjects/ParticleQuantityResidualAccumulatorBase.h
@@ -0,0 +1,57 @@
+//* This file is part of SALAMANDER: Software for Advanced Large-scale Analysis of MAgnetic confinement for Numerical Design, Engineering & Research,
+//* A multiphysics application for modeling plasma facing components
+//* https://github.com/idaholab/salamander
+//* https://mooseframework.inl.gov/salamander
+//*
+//* SALAMANDER is powered by the MOOSE Framework
+//* https://www.mooseframework.inl.gov
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+//*
+//* Copyright 2025, Battelle Energy Alliance, LLC
+//* ALL RIGHTS RESERVED
+//*
+
+#pragma once
+#include "GeneralUserObject.h"
+// including ray for RayDataIndex
+#include "Ray.h"
+class PICStudyBase;
+
+class ParticleQuantityResidualAccumulatorBase : public GeneralUserObject
+{
+public:
+  static InputParameters validParams();
+
+  ParticleQuantityResidualAccumulatorBase(const InputParameters & params);
+
+  /**
+   * Unused methods
+   */
+  ///@{
+  virtual void initialize() override {}
+  virtual void finalize() override {}
+  ///@}
+
+protected:
+  /// the name of the variable which are contributing to the residual of
+  const NonlinearVariableName _var_name;
+  /// the PIC study that is tracking all of our particles
+  const PICStudyBase & _study;
+  /// the ray data index where each of the velocity components are stored
+  ///@{
+  const RayDataIndex _v_x_index;
+  const RayDataIndex _v_y_index;
+  const RayDataIndex _v_z_index;
+  ///@}
+  /// the ray data index where the weight of the particle is stored
+  const RayDataIndex _weight_index;
+  /// the ray data index where the charge of the particle is stored
+  const RayDataIndex _charge_index;
+  /// the ray data index where the mass of the particle is stored
+  const RayDataIndex _mass_index;
+  /// the ray data index where the species of the particle is stored
+  const RayDataIndex _species_index;
+
+};
---
+++ src/userobjects/ParticleQuantityResidualAccumulatorBase.C
@@ -0,0 +1,51 @@
+//* This file is part of SALAMANDER: Software for Advanced Large-scale Analysis of MAgnetic confinement for Numerical Design, Engineering & Research,
+//* A multiphysics application for modeling plasma facing components
+//* https://github.com/idaholab/salamander
+//* https://mooseframework.inl.gov/salamander
+//*
+//* SALAMANDER is powered by the MOOSE Framework
+//* https://www.mooseframework.inl.gov
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+//*
+//* Copyright 2025, Battelle Energy Alliance, LLC
+//* ALL RIGHTS RESERVED
+//*
+
+#include "ParticleQuantityResidualAccumulatorBase.h"
+#include "PICStudyBase.h"
+
+InputParameters
+ParticleQuantityResidualAccumulatorBase::validParams()
+{
+  auto params = GeneralUserObject::validParams();
+  params.addClassDescription(
+      "Accumulator used to evaluate the inner product of the particle number density and the test function.");
+  params.addParam<UserObjectName>("study", "study", "The PICStudy that owns the charged particles");
+  // These parameters are necessary when using ResidualAccumulator
+  params += TaggingInterface::validParams();
+  // This exec flag is necessary for the ParticleQuantityResidualAccumulatorBase to contribute to residuals
+  ExecFlagEnum & exec_enum = params.set<ExecFlagEnum>("execute_on", true);
+  exec_enum.addAvailableFlags(EXEC_PRE_KERNELS);
+  params.set<ExecFlagEnum>("execute_on") = EXEC_PRE_KERNELS;
+  // making this input parameter private so the user cannot use the object incorrectly
+  params.suppressParameter<ExecFlagEnum>("execute_on");
+  params.addRequiredParam<NonlinearVariableName>("variable",
+                                                 "The variable to contribute to the residual of");
+  return params;
+}
+
+ParticleQuantityResidualAccumulatorBase::ParticleQuantityResidualAccumulatorBase(const InputParameters & params)
+  : GeneralUserObject(params),
+    _var_name(getParam<NonlinearVariableName>("variable")),
+    _study(getUserObject<PICStudyBase>("study")),
+    _v_x_index(_study.getRayDataIndex("v_x")),
+    _v_y_index(_study.getRayDataIndex("v_y")),
+    _v_z_index(_study.getRayDataIndex("v_z")),
+    _weight_index(_study.getRayDataIndex("weight")),
+    _charge_index(_study.getRayDataIndex("charge")),
+    _mass_index(_study.getRayDataIndex("mass")),
+    _species_index(_study.getRayDataIndex("species"))
+{
+}
