06be24070f05fc8bebb6aa6da0299a7bb11208b4
Casey Icenhour <cticenhour@gmail.com>
2024-08-28 08:48:59 -0600

    Apply suggestions from code review
    Co-authored-by: Pierre-Clement Simon <pierreclement.simon@gmail.com>

--- config/check_deps.mk
+++ config/check_deps.mk
@@ -11,7 +11,7 @@ ENABLE_CARDINAL      := yes
 CARDINAL_CONTENT     := $(shell ls $(CARDINAL_DIR) 2> /dev/null)
 
 ifeq ($(CARDINAL_CONTENT),)
-  $(warning $n"Cardinal does not seem to be available. If usage of Cardinal is desired within SALAMANDER, make sure that either the submodule is checked out$nor that CARDINAL_DIR points to a location with the Cardinal source.")
+  $(warning $n"Cardinal does not seem to be available. If usage of Cardinal is desired within SALAMANDER, make sure that either the submodule is checked out$nor that CARDINAL_DIR points to a location with the Cardinal source. $n In the meantime, SALAMANDER will be built without Cardinal.")
   ENABLE_CARDINAL    := no
 else
   $(info SALAMANDER is using Cardinal from     $(CARDINAL_DIR))
