6b4362780507e2b8a23db32c483b1522887d151e
Casey Icenhour <casey.icenhour@inl.gov>
2025-07-13 15:14:03 -0600

    Add embree and doubledown as new dependencies for cardinal
    refs neams-th-coe/cardinal#1130
    
    Disable them by default for now, per neams-th-coe/cardinal#1142
    
    Also add new moose physics modules required for a build including cardinal

--- config/dep_modules.mk
+++ config/dep_modules.mk
@@ -19,9 +19,11 @@ ifeq ($(ENABLE_CARDINAL),yes)
   FLUID_PROPERTIES    := yes
   HEAT_TRANSFER       := yes
   NAVIER_STOKES       := yes
+  RAY_TRACING         := yes
   REACTOR             := yes
+  SOLID_MECHANICS     := yes
   SOLID_PROPERTIES    := yes
   STOCHASTIC_TOOLS    := yes
-  SOLID_MECHANICS     := yes
+  SUBCHANNEL          := yes
   THERMAL_HYDRAULICS  := yes
 endif
