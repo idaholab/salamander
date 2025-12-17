f46a28084706876a36f2df8d10bfcf7d746f904c
Grayson Gall <ggall713@gmail.com>
2025-12-17 16:13:51 -0500

    Cleaning up python scripts and add documentation for verification and in scripts
    - Added the verificiation documentation page for the stepper
      verification pages.
    - Added function documentation and comments to all of the python scripts
      that are used during testing.
    - Decided to keep the convergence studies as heavy tests since they took
      about 5-6 seconds when run on my machine.

--- test/tests/userobjects/particle_stepper/leapfrog_stepper/test.py
+++ test/tests/userobjects/particle_stepper/leapfrog_stepper/test.py
@@ -1,11 +1,11 @@
-#!/usr/bin/env python
 import sys
 import unittest
 
+# enabling the file to call the required common functions
 sys.path.append("../")
 from stepper_analysis import test_case
 
 
-class LeapFrogCyclotronElectricTest(unittest.TestCase):
+class LeapFrogTimeVarryingElectricTest(unittest.TestCase):
     def test(self):
         test_case("circular_e_field.i")
